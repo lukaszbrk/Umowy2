0fa09061f4fa74c06a5ba82870c7a4c1774dfe9e
Uki <borkowskil@outlook.com>
2019-07-20 23:08:36 +0200

    E.g. done

--- src/example/Columns.js
+++ src/example/Columns.js
@@ -5,46 +5,89 @@ import {
   Image,
   Segment,
   Divider,
-  Pagination
+  Pagination,
+  Button,
+  Icon
 } from "semantic-ui-react";
 import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+const pStyle = {
+  padding: "10px",
+  paddingBottom: "50px",
+  border: "1px solid blue"
+};
+
+function objectLength(obj) {
+  var result = 0;
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      result++;
+    }
+  }
+  return result;
+}
 
 const Columns = ({ data, selectedClause }) => {
 
-//const use =  ()=>resetPagination? 0:{}
   const [exampleNumber, setNumber] = useState(0);
 
   const onPageChange = (e, { activePage }) => {
-    console.log("exampleNumber: "+exampleNumber)
-    console.log("activePage: " +activePage)
-    setNumber(activePage-1);
+    console.log("exampleNumber: " + exampleNumber);
+    console.log("activePage: " + activePage);
+    setNumber(activePage - 1);
+  };
+
+  const returnTotalPages = () => {
+    if (selectedClause) {
+      //console.log(objectLength(data["Clauses"][selectedClause]["Examples"]))
+      return objectLength(data["Clauses"][selectedClause]["Examples"]);
+    } else {
+      return 0;
+    }
   };
 
   return (
     <Grid.Column width={10}>
       <Segment>
-  
         <Pagination
           boundaryRange={0}
-    
           defaultActivePage={0}
           ellipsisItem={null}
           firstItem={null}
           lastItem={null}
-       //TODO get objects numbers for total Pages
-          totalPages={2}
+        
+          totalPages={returnTotalPages()}
           onPageChange={onPageChange}
         />
-            <Divider />
+        <Divider />
         <Grid divided="vertically">
           <Grid.Row columns={2}>
             <Grid.Column>
               {selectedClause ? (
                 <Container textAlign="justified">
-                  <p>
+                  <p
+                    contentEditable="true"
+                    onClick={() => console.log("klik")}
+                    style={pStyle}
+                  >
                     {
-                      data["Clauses"][selectedClause]["Examples"][exampleNumber]["pl"]
+                      data["Clauses"][selectedClause]["Examples"][
+                        exampleNumber
+                      ]["pl"]
                     }
+                    <CopyToClipboard
+                      text={
+                        data["Clauses"][selectedClause]["Examples"][
+                          exampleNumber
+                        ]["pl"]
+                      }
+                    >
+                      <Button size="mini" basic floated="right">
+                        <Icon name="copy" />
+                        Kopiuj
+                      </Button>
+                    </CopyToClipboard>
                   </p>
                 </Container>
               ) : (
@@ -54,11 +97,28 @@ const Columns = ({ data, selectedClause }) => {
             <Grid.Column>
               {selectedClause ? (
                 <Container textAlign="justified">
-                  <p>
+                  <p
+                    contentEditable="true"
+                    onClick={() => console.log("klik")}
+                    style={pStyle}
+                  >
                     {
-                      data["Clauses"][selectedClause]["Examples"][exampleNumber]["eng"]
-                      
+                      data["Clauses"][selectedClause]["Examples"][
+                        exampleNumber
+                      ]["eng"]
                     }
+                    <CopyToClipboard
+                      text={
+                        data["Clauses"][selectedClause]["Examples"][
+                          exampleNumber
+                        ]["eng"]
+                      }
+                    >
+                      <Button size="mini" basic floated="right">
+                        <Icon name="copy" />
+                        Copy
+                      </Button>
+                    </CopyToClipboard>
                   </p>
                 </Container>
               ) : (
@@ -67,7 +127,6 @@ const Columns = ({ data, selectedClause }) => {
             </Grid.Column>
           </Grid.Row>
         </Grid>
-       
       </Segment>
     </Grid.Column>
   );
