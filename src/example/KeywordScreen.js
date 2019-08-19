3aaacaec785a84b936c24251af55ae163ddce3f4
Uki <borkowskil@outlook.com>
2019-08-19 11:52:20 +0200

    Update KeywordScreen.js
    useffect

--- src/example/KeywordScreen.js
+++ src/example/KeywordScreen.js
@@ -3,59 +3,64 @@
 import React from "react";
 import { List } from "semantic-ui-react";
 
-const KeywordScreen = ({ data}) => {
+import { useState, useEffect  } from "react";
 
-  console.log(data)
+const KeywordScreen = ({ data }) => {
+
+
+
+
+  const [ListNumber, setListNumber] = useState("");
+
+  
+  useEffect(() => {
+
+
+   setListNumber("")
+    //
+  }, [data["selectedKeyword"]]);
+
+
+    const onClikedItem = e => {
+    //console.log(e.currentTarget.id);
+    setListNumber(e.currentTarget.id);
+  };
+
+ 
 
   function Test(data) {
-
-    
-
-     //console.log(data["_clauseDescription"].length)
-
-     let rndrdList = []
-     for (var i = 0; i < data["_clauseDescription"].length; i++) {
-       rndrdList.push( 
-         
- 
-         <List.Item key={data["_clauseDescription"][i][0]}>
-        
-         <List.Content>
-           <List.Header > {data["_clauseDescription"][i][0]}</List.Header>
-           
-           <List.Description >{data["_clauseDescription"][i][1]}</List.Description>
-
-           <p>Liczba przykładów: {data["aux2"][data["_clauseDescription"][i][0]]}</p>
-  
-         </List.Content>
-       </List.Item>)}
-       
-      return rndrdList
-      };
   
 
-  /*
-  const  RenderList = (data) =>{
-
-    let rndrdList = []
+    let rndrdList = [];
     for (var i = 0; i < data["_clauseDescription"].length; i++) {
-      rndrdList.push( 
-        
+      rndrdList.push(
+        <List.Item
+          key={data["_clauseDescription"][i][0]}
+          onClick={onClikedItem}
+          id={data["_clauseDescription"][i][0]}
+        >
+          <List.Content>
+            <List.Header> {data["_clauseDescription"][i][0]}</List.Header>
 
-        <List.Item>
-        <List.Icon name='github' size='large' verticalAlign='middle' />
-        <List.Content>
-          <List.Header as='a'> {data["_clauseDescription"][i]}</List.Header>
-          <List.Description as='a'>Description</List.Description>
-        </List.Content>
-      </List.Item>)
-    
+            <List.Description>
+              {data["_clauseDescription"][i][1]}
+            </List.Description>
+
+            <p>
+              Liczba przykładów:{" "}
+              {data["aux2"][data["_clauseDescription"][i][0]]}
+            </p>
+          </List.Content>
+        </List.Item>
+      );
     }
 
-    return rndrdList
+    //add/replace "old" keyword for state
 
-  };
-*/
+    
+    return rndrdList;
+  }
+
   for (var i = 0; i < data["dataforColumns"].length; i++) {
     //console.log("Data for columns: " + data["dataforColumns"][i][1]);
   }
@@ -63,20 +68,23 @@ const KeywordScreen = ({ data}) => {
   for (var i = 0; i < data["dataforColumns"].length; i++) {
     //console.log("Data for columns: " + data["dataforColumns"][i][1]);
     //console.log("Data for columns: " + data["dataforColumns"][i][0]);
-
   }
 
   for (var i = 0; i < data["_clauseDescription"].length; i++) {
-    //console.log("Clauses: " + data["_clauseDescription"][i][0]); 
+    //console.log("Clauses: " + data["_clauseDescription"][i][0]);
   }
   return (
     <div>
       <p>Keywords Screen</p>
       <p>{"Number of pairs: " + data["dataforColumns"].length}</p>
 
-     
+      {/*
       <List divided relaxed>{Test(data)}</List>
- 
+      */}
+
+       {
+      (!ListNumber)? <List divided relaxed>{Test(data)}</List> : <p>Column Screen</p>
+      }
     </div>
   );
 };
